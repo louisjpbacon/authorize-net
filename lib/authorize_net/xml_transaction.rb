1cb9f6ccbc49ed7bc050c04050b089dc19263105
Louis Bacon <louis@homeflow.co.uk>
2015-09-03 21:35:11 +0100

    Updated API URL

--- lib/authorize_net/xml_transaction.rb
+++ lib/authorize_net/xml_transaction.rb
@@ -2,16 +2,16 @@ module AuthorizeNet
 
   # The ARB transaction class.
   class XmlTransaction < AuthorizeNet::Transaction
-    
+
     # The XML namespace used by the ARB API.
     XML_NAMESPACE = 'AnetApi/xml/v1/schema/AnetApiSchema.xsd'
-    
+
     # Constants for both the various Authorize.Net subscription gateways are defined here.
     module Gateway
-      LIVE = 'https://api.authorize.net/xml/v1/request.api'
+      LIVE = 'https://api2.authorize.net/xml/v1/request.api'
       TEST = 'https://apitest.authorize.net/xml/v1/request.api'
     end
-    
+
     # Constants for both the various Authorize.Net transaction types are defined here.
     module Type
       ARB_CREATE = "ARBCreateSubscriptionRequest"
@@ -39,38 +39,38 @@ module AuthorizeNet
       REPORT_GET_UNSETTLED_TRANSACTION_LIST = "getUnsettledTransactionListRequest"
       REPORT_GET_TRANSACTION_DETAILS = "getTransactionDetailsRequest"
     end
-    
+
     # Fields to convert to/from booleans.
     @@boolean_fields = []
-    
+
     # Fields to convert to/from BigDecimal.
     @@decimal_fields = []
-    
+
     # Fields to convert to/from Date.
     @@date_fields = []
-    
+
     # Fields to convert to/from DateTime.
     @@datetime_fields = []
-    
+
     # The class to wrap our response in.
     @response_class = AuthorizeNet::XmlResponse
-    
+
     # The default options for the constructor.
     @@option_defaults = {
       :gateway => :production,
       :verify_ssl => false,
       :reference_id => nil
     }
-    
+
     # DO NOT USE. Instantiate AuthorizeNet::ARB::Transaction or AuthorizeNet::CIM::Transaction instead.
     def initialize(api_login_id, api_transaction_key, options = {})
       super()
       @api_login_id = api_login_id
       @api_transaction_key = api_transaction_key
-      
+
       @response ||= nil
       @type ||= nil
-      
+
       options = @@option_defaults.merge(options)
       @verify_ssl = options[:verify_ssl]
       @reference_id = options[:reference_id]
@@ -83,38 +83,38 @@ module AuthorizeNet
         @gateway = options[:gateway]
       end
     end
-    
+
     # Checks if the transaction has been configured for the sandbox or not. Return FALSE if the
     # transaction is running against the production, TRUE otherwise.
     def test?
       @gateway != Gateway::LIVE
     end
-        
+
     # Checks to see if the transaction has a response (meaning it has been submitted to the gateway).
     # Returns TRUE if a response is present, FALSE otherwise.
     def has_response?
       !@response.nil?
     end
-    
+
     # Retrieve the response object (or Nil if transaction hasn't been sent to the gateway).
     def response
       @response
     end
-    
+
     # Submits the transaction to the gateway for processing. Returns a response object. If the transaction
     # has already been run, it will return nil.
     def run
       make_request
     end
-    
+
     # Returns a deep-copy of the XML object sent to the payment gateway. Or nil if there was no XML payload.
     def xml
       @xml
     end
-    
+
     #:enddoc:
     protected
-    
+
     # Takes a list of nodes (a Hash is a node, and Array is a list) and returns True if any nodes
     # would be built by build_nodes. False if no new nodes would be generated.
     def has_content(nodeList, data)
@@ -148,7 +148,7 @@ module AuthorizeNet
       end
       false
     end
-    
+
     # Takes a list of nodes (a Hash is a node, and Array is a list) and recursively builds the XML by pulling
     # values as needed from data.
     def build_nodes(builder, nodeList, data)
@@ -187,7 +187,7 @@ module AuthorizeNet
         end
       end
     end
-    
+
     def convert_field(field, value)
       if @@boolean_fields.include?(field) and !value.nil?
         return boolean_to_value(value)
@@ -211,7 +211,7 @@ module AuthorizeNet
             options <<= self.to_param(key, @custom_fields[key.to_sym], '')
           end
         end
-        
+
         if options.length > 0
           return options.join('&')
         else
@@ -228,18 +228,18 @@ module AuthorizeNet
           end
         end
       end
-      
+
       value
     end
-    
+
     # An internal method that builds the POST body, submits it to the gateway, and constructs a Response object with the response.
     def make_request
       if has_response?
         return nil
       end
-      
+
       fields = @fields
-      
+
       builder = Nokogiri::XML::Builder.new(:encoding => 'utf-8') do |x|
         x.send(@type.to_sym, :xmlns => XML_NAMESPACE) {
           x.merchantAuthentication {
@@ -250,9 +250,9 @@ module AuthorizeNet
         }
       end
       @xml = builder.to_xml
-      
+
       url = URI.parse(@gateway)
-      
+
       request = Net::HTTP::Post.new(url.path)
       request.content_type = 'text/xml'
       request.body = @xml
@@ -263,7 +263,7 @@ module AuthorizeNet
       else
         connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
-      
+
       # Use our Class's @response_class variable to find the Response class we are supposed to use.
       begin
         @response = self.class.instance_variable_get(:@response_class).new((connection.start {|http| http.request(request)}), self)
@@ -271,6 +271,6 @@ module AuthorizeNet
         @response = self.class.instance_variable_get(:@response_class).new($!, self)
       end
     end
-    
+
   end
 end
